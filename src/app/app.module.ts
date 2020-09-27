5a48c2e72f95eb0d66d42d43fe738925e92d3c3d
“Jessy” <jmpatino34@misena.edu.co>
2020-09-27 18:38:57 -0500

    se agregaron: imagenes, barra de navegacion, se le puso navegacion a inicio y empresa y se añadio jquery y popper js.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedComponent } from './components/shared/shared.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { QuienesComponent } from './components/quienes/quienes.component';
+import { MenuComponent } from './templates/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent,
+    NavbarComponent,
+    HomeComponent,
+    QuienesComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
